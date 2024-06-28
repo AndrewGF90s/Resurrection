debd52293b7c5606de6190e0d4cf37581ccb7335
ANdy <galchok87@gmail.com>
2024-06-28 18:17:39 +0300

    add paractic

--- Resurrection/src/program.java
+++ Resurrection/src/program.java
@@ -1,9 +1,22 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
 public class program {
     public static void main(String[] args) {
-        System.out.println(" Привет!!!");
+        Scanner scr = new Scanner(System.in);
+        System.out.print("Введите имя");
+        String name = scr.nextLine();
+        System.out.printf("Сколько тебе лет?");
+        boolean test  = scr.hasNextInt();
+        int age = scr.nextInt();
+            System.out.print(name  +"  Ты молодец!" + " Скоро тебе будет уже " + (age + 1) +"  Пора рости!!!" );
+
+//        System.out.println("Привет " + name + "!!!");
 
 
     }
+
 
 
 }
