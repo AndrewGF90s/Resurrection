e59bc9bccdb9976f1aa3419af405cd9a2a304bc1
ANdy <galchok87@gmail.com>
2024-07-01 20:33:29 +0300

    add homework1

--- Resurrection/src/Main.java
+++ Resurrection/src/Main.java
@@ -1,11 +1,32 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Program.realtime());
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("That is your name ?");
-        String name = scanner.nextLine();
-        System.out.println("Привет, " + name);
+        int[] array = new int[]{1, 2, 3, 4, 5, 5, 6, 3, 7, 8, 9, 3, 3, 2, 3, 3, 1, 647, 123, 0, 213,};
+        int num = 3;
+        System.out.println(Arrays.toString(changeArray(array, num)));
+
+    }
+
+    public static int[] changeArray(int[] newArray, int number) {
+        int position = newArray.length - 1;
+        for (int i = 0; i < position; i++) {
+            if (newArray[i] == number) {
+                while (newArray[position] == number && position > i) {
+                    position--;
+
+                }
+                int temp = newArray[i];
+                newArray[i] = newArray[position];
+                newArray[position] = temp;
+                position--;
+            }
+
+        }
+
+        return newArray;
     }
 }
+
+
