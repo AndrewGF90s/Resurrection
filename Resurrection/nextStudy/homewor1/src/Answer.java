91f1f54edb9395c60399dac943c3dce9c833e5ad
ANdy <galchok87@gmail.com>
2024-07-01 20:41:57 +0300

    change homework

--- Resurrection/nextStudy/homewor1/src/Answer.java
+++ Resurrection/nextStudy/homewor1/src/Answer.java
@@ -2,16 +2,19 @@ import java.util.Scanner;
 
 public class Answer {
     public static void main(String[] args) {
-        System.out.println("Введите число для УС/" +
-                "ТРЕУГОЛЬНЕНИЯ ");
-        int  num = new Scanner(System.in).nextInt();
-        System.out.println(countTriagleN(num));
-    }
-    public static int countTriagleN(int number) {
-        int answ =0;
+        System.out.println("Введите число для УТРЕУГОЛЬНЕНИЯ ");
+        int num = new Scanner(System.in).nextInt();
 
+
+        System.out.println("результат будет равен " +countTriagleN(num));
+    }
+
+    public static int countTriagleN(int number) {
+        if (number<1)
+            return-1;
+        int answ = 0;
         for (int i = 0; i <= number; i++) {
-             answ += i;
+            answ += i;
         }
         return answ;
 
